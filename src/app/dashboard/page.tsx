a6778d84189c15cd4469c06ad341dac863999525
GitHub225-68 <ritu618@gmail.com>
2024-03-16 10:57:34 -0700

    google oauth fix

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -5,6 +5,7 @@ import { Fragment } from 'react'
 import { Disclosure, Menu, Transition } from '@headlessui/react'
 import { Bars3Icon, BellIcon, XMarkIcon } from '@heroicons/react/24/outline'
 import { signOut, useSession } from "next-auth/react";
+import { StaticImport } from "next/dist/shared/lib/get-img-props";
 
 
 const navigation = [
@@ -26,6 +27,7 @@ function classNames(...classes:string[]) {
 
 export default function Dashboard() {
   const { data: session, status } = useSession()
+  const userImage:any = session?.user?.image
 
   if (status === "unauthenticated") {
     return <p>You are not logged in</p>
@@ -91,7 +93,7 @@ export default function Dashboard() {
                             <Menu.Button className="relative flex max-w-xs items-center rounded-full bg-navy text-sm focus:outline-none focus:ring-2 focus:ring-white focus:ring-offset-2 focus:ring-offset-gray-800">
                                 <span className="absolute -inset-1.5" />
                                 <span className="sr-only">Open user menu</span>
-                                <Image className="rounded-full" src={session?.user?.image} width={30} height={30} alt="" />
+                                <Image className="rounded-full" src={userImage} width={30} height={30} alt="" />
                             </Menu.Button>
                             </div>
                             <Transition
@@ -167,7 +169,7 @@ export default function Dashboard() {
                     <div className="pb-3 pt-4">
                     <div className="flex items-center px-5">
                         <div className="flex-shrink-0">
-                            <Image className="rounded-full" src={session?.user?.image} width={30} height={30} alt="" />
+                            <Image className="rounded-full" src={userImage} width={30} height={30} alt="" />
                         </div>
                         <div className="ml-3">
                             <div className="text-base font-medium leading-none text-white">{session?.user?.name}</div>
