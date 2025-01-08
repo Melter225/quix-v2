eea3d9edddd5ed2c31d4e2d7b8023896bc47db2c
Melter225 <114307943+Melter225@users.noreply.github.com>
2025-01-08 12:38:27 -0800

    Fix type errors (Pt. 9)

--- lib/db.ts
+++ lib/db.ts
@@ -1,15 +1,26 @@
-import { PrismaClient } from '@prisma/client'
+import { MongoClient } from "mongodb";
+import { PrismaClient } from "@prisma/client";
+
+let client: MongoClient;
+
+export async function connectToDb() {
+  if (!client) {
+    client = new MongoClient(process.env.MONGODB_URI!);
+    await client.connect();
+  }
+  return client.db(process.env.MONGODB_DB);
+}
 
 const prismaClientSingleton = () => {
-  return new PrismaClient()
-}
+  return new PrismaClient();
+};
 
 declare const globalThis: {
   prismaGlobal: ReturnType<typeof prismaClientSingleton>;
 } & typeof global;
 
-const prisma = globalThis.prismaGlobal ?? prismaClientSingleton()
+const prisma = globalThis.prismaGlobal ?? prismaClientSingleton();
 
-export default prisma
+export default prisma;
 
-if (process.env.NODE_ENV !== 'production') globalThis.prismaGlobal = prisma
+if (process.env.NODE_ENV !== "production") globalThis.prismaGlobal = prisma;
